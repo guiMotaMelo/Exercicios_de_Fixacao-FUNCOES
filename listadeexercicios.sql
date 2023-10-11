f7bb949635fdc8d5b996831f4c26e1df95895aca
yMotaBR <guiguimota2017@gmail.com>
2023-10-11 06:40:04 -0300

    Funções Numéricas:a

--- listadeexercicios.sql
+++ listadeexercicios.sql
@@ -18,3 +18,14 @@ SELECT
         ELSE 'Sra. ' || nome
     END AS nome_com_tratamento
 FROM nomes;
+
+CREATE TABLE produtos (
+    produto VARCHAR(50),
+    preco DECIMAL(10, 2),
+    quantidade INT
+);
+
+INSERT INTO produtos (produto, preco, quantidade) VALUES
+    ('Produto A', 10.99, 5),
+    ('Produto B', 5.49, 0),
+    ('Produto C', 15.99, -2);
